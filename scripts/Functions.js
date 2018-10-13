161a5412a942b7725dec30f366572c8b0b4f33a1
Χρήστος Ταφαρλής <32601648+XristosTafarlis@users.noreply.github.com>
2018-10-14 00:25:25 +0300

    820

--- scripts/Functions.js
+++ scripts/Functions.js
@@ -29,7 +29,7 @@ function launchEnemies1() {
 	var enemy = enemies1.getFirstExists(false);
 	if (enemy) {
 		//enemy.reset( 820, game.rnd.integerInRange (0, 600));
-		enemy.reset (400, 300);
+		enemy.reset (820, 300);
 		//enemy.body.velocity.y = game.rnd.integerInRange(-300, 300);
 		enemy.body.velocity.x = ENEMY_SPEED;
 		//enemy.body.drag.y = 100;
