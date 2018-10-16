0a6dbfff27e42a32e42a28167fb5b6041ff8d876
Χρήστος Ταφαρλής <32601648+XristosTafarlis@users.noreply.github.com>
2018-10-16 10:53:33 +0300

    main menu abjustments

--- scripts/MainMenu.js
+++ scripts/MainMenu.js
@@ -2,20 +2,20 @@ var MainMenu = {
 	create: function(){
 		this.stage.backgroundColor = '#000020';
 
-		//Creating Super Mario
-		var Shooter = game.add.image(400, 80, 'title');
+		//Creating Title
+		var Shooter = game.add.image(600, 150, 'title');
 		Shooter.scale.set(2, 2);
 		Shooter.anchor.set(0.5, 0.5);
 		
 		//Creating level 1 button
-		var buttonLVL1 = game.add.button(400 , 200, "lvl1", function(){
+		var buttonLVL1 = game.add.button(600 , 350, "lvl1", function(){
 			game.state.start('GamePlay');
 		});
 		buttonLVL1.scale.set(2, 2);
 		buttonLVL1.anchor.set(0.5, 0.5);
 
 		//Creating level 2 button
-		var buttonLVL2 = game.add.button(400 , 300, "lvl2", function(){
+		var buttonLVL2 = game.add.button(600 , 500, "lvl2", function(){
 			game.state.start('GamePlay');
 		});
 		buttonLVL2.scale.set(2, 2);
